1a1997dee9d3dff01a109b4905fd5991673daaba
Francisco Bórquez <fborquez@outlook.com>
2024-04-25 21:43:55 -0400

    fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   aangBaseUrl: 'https://aang-7s4sq47lha-rj.a.run.app/api/',
-  backendUrl: 'https://katara-back-7s4sq47lha-rj.a.run.app'
+  backendUrl: 'https://katara-back-7s4sq47lha-rj.a.run.app/api/'
 };
