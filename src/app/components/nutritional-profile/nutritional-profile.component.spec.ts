2db9eacea40b0279194ec1bdf0860b2fa2c2fb5d
Francisco Bórquez <fborquez@outlook.com>
2024-08-22 20:26:23 -0400

    fix unit tests

--- src/app/components/nutritional-profile/nutritional-profile.component.spec.ts
+++ src/app/components/nutritional-profile/nutritional-profile.component.spec.ts
@@ -5,12 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormBuilder, FormGroupDirective, ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 
 describe('NutritionalProfileComponent', () => {
   let component: NutritionalProfileComponent;
   let fixture: ComponentFixture<NutritionalProfileComponent>;
 
+  const fb = new FormBuilder()
   const formGroupDirective = new FormGroupDirective([], []);
+  formGroupDirective.form = fb.group({
+    test: fb.control(null)
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,7 +23,8 @@ describe('NutritionalProfileComponent', () => {
         HttpClientModule,
         MatSnackBarModule,
         ReactiveFormsModule,
-        MatSelectModule
+        MatSelectModule,
+        MatTableModule
       ],
       providers: [
         FormGroupDirective,
