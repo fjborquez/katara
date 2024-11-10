721482b151daeff2ed899c3881b2c4515e7fc522
Francisco Bórquez <fborquez@outlook.com>
2024-11-10 11:23:34 -0300

    fix unit tests

--- src/app/sections/house-inventory-view/house-inventory-view.component.spec.ts
+++ src/app/sections/house-inventory-view/house-inventory-view.component.spec.ts
@@ -1,19 +1,43 @@
+import { MatSelectModule } from '@angular/material/select';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HouseInventoryViewComponent } from './house-inventory-view.component';
+import { InventoryHousesService } from '../../services/inventory-houses.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
 
 describe('HouseInventoryViewComponent', () => {
   let component: HouseInventoryViewComponent;
   let fixture: ComponentFixture<HouseInventoryViewComponent>;
+  let inventoryHousesService: InventoryHousesService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HouseInventoryViewComponent ]
+      declarations: [ HouseInventoryViewComponent ],
+      imports: [
+        HttpClientTestingModule,
+        MatSelectModule,
+        MatSnackBarModule,
+        MatTableModule
+      ],
+      providers: [ HouseInventoryViewComponent, {
+        provide: ActivatedRoute,
+        useValue: {
+          snapshot: {
+            params: {
+              'id': '1'
+            }
+          }
+        }
+      }]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(HouseInventoryViewComponent);
     component = fixture.componentInstance;
+    inventoryHousesService = fixture.debugElement.injector.get(InventoryHousesService);
     fixture.detectChanges();
   });
 
--- src/app/services/inventory-houses.service.spec.ts
+++ src/app/services/inventory-houses.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { InventoryHousesService } from './inventory-houses.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('InventoryHousesService', () => {
   let service: InventoryHousesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ]
+    });
     service = TestBed.inject(InventoryHousesService);
   });
 
