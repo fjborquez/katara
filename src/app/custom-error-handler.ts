8dc65a7e89aeea8e71c8486e4e70713a02c13c31
Francisco Bórquez <fborquez@outlook.com>
2024-12-15 10:30:07 -0300

    asd

--- src/app/custom-error-handler.ts
+++ src/app/custom-error-handler.ts
@@ -1,26 +1,17 @@
+import { LogWriterService } from './services/log-writer.service';
 import { ErrorHandler, Injectable, isDevMode } from '@angular/core';
-import { Logging } from '@google-cloud/logging';
 
 @Injectable()
 export class CustomErrorHandler implements ErrorHandler {
+  constructor(
+    private logWriterService: LogWriterService
+  ) { }
+
   handleError(error: any): void {
     if (isDevMode()) {
       console.error(error);
     } else {
-      logToGoogle(error, 'avatar-419000').catch(console.error);
+      this.logWriterService.add(error);
     }
   }
-}
-
-async function logToGoogle(error: any, projectId: string, logName = 'katara-log') {
-  const logging = new Logging({projectId});
-  const log = logging.log(logName);
-  const metadata = {
-    resource: {
-      type: 'global'
-    },
-    severity: 'ERROR'
-  };
-  const entry = log.entry(metadata, error);
-  await log.write(entry);
 }
---
+++ src/app/services/log-writer.service.spec.ts
@@ -0,0 +1,19 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { CityService } from './city.service';
+
+describe('CityService', () => {
+  let service: CityService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(CityService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
