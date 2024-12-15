6a52eda404b902eb15f673f9849f36df12534937
Francisco Bórquez <fborquez@outlook.com>
2024-12-15 08:36:15 -0300

    log to google

--- src/app/custom-error-handler.ts
+++ src/app/custom-error-handler.ts
@@ -1,12 +1,26 @@
 import { ErrorHandler, Injectable, isDevMode } from '@angular/core';
+import { Logging } from '@google-cloud/logging';
 
 @Injectable()
 export class CustomErrorHandler implements ErrorHandler {
   handleError(error: any): void {
-    if (!isDevMode()) {
-      console.log('WE ARE IN PROD MODE');
+    if (isDevMode()) {
+      console.error(error);
+    } else {
+      logToGoogle(error, 'avatar-419000').catch(console.error);
     }
-
-    console.error(error);
   }
+}
+
+async function logToGoogle(error: any, projectId: string, logName = 'katara-log') {
+  const logging = new Logging({projectId});
+  const log = logging.log(logName);
+  const metadata = {
+    resource: {
+      type: 'global'
+    },
+    severity: 'ERROR'
+  };
+  const entry = log.entry(metadata, error);
+  await log.write(entry);
 }
