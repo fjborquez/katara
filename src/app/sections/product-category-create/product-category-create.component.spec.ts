22e34a523bc52d6f951c403de4f323eaaa85d2bc
Francisco Bórquez <fborquez@outlook.com>
2024-12-18 21:53:09 -0300

    test

--- src/app/sections/product-category-create/product-category-create.component.spec.ts
+++ src/app/sections/product-category-create/product-category-create.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductCategoryCreateComponent } from './product-category-create.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('ProductCategoryCreateComponent', () => {
   let component: ProductCategoryCreateComponent;
@@ -8,7 +13,8 @@ describe('ProductCategoryCreateComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductCategoryCreateComponent ]
+      declarations: [ ProductCategoryCreateComponent ],
+      imports: [MatSnackBarModule, HttpClientTestingModule, MatAutocompleteModule, RouterTestingModule, FormsModule, ReactiveFormsModule]
     })
     .compileComponents();
 
