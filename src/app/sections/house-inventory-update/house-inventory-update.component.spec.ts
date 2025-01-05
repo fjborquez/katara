ea33c1dcd983e0718e6272bd11873055e094e117
Francisco Borquez <fborquez@outlook.com>
2025-01-05 17:13:59 -0300

    fix testing

--- src/app/sections/house-inventory-update/house-inventory-update.component.spec.ts
+++ src/app/sections/house-inventory-update/house-inventory-update.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HouseInventoryUpdateComponent } from './house-inventory-update.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HouseInventoryUpdateComponent', () => {
   let component: HouseInventoryUpdateComponent;
@@ -8,7 +14,16 @@ describe('HouseInventoryUpdateComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HouseInventoryUpdateComponent ]
+      declarations: [ HouseInventoryUpdateComponent ],
+      imports: [
+        RouterTestingModule,
+        MatSnackBarModule,
+        HttpClientTestingModule,
+        MatSelectModule,
+        MatAutocompleteModule,
+        ReactiveFormsModule,
+        FormsModule
+      ]
     })
     .compileComponents();
 
